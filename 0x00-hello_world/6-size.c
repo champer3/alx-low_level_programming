b2a4d5211aabededd0dd137703f34cf403852bf7
champer3 <okunolas21@gmail.com>
2021-02-03 15:52:04 +0000

    Low level programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Entry
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 char acharacter;
@@ -11,10 +12,11 @@ int ainteger;
 long along;
 long long alonglong;
 float afloat;
+
 printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
 printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
 printf("Size of a long int: %lu byte(s)\n", sizeof(along));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+printf("Size of a long long int: %lu bytes(s)\n", sizeof(alonglong));
 printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
 return (0);
 }
