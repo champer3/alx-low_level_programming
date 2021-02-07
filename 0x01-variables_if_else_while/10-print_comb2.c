157d9f5da957b9f2b7d7fbd7e2cef61d71c2f245
champer3 <okunolas21@gmail.com>
2021-02-07 13:22:52 +0000

    a little change

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -9,6 +9,7 @@ int main(void)
 {
 int tens;
 int ones;
+
 for (tens = 0; tens < 10; tens++)
 {
 for (ones = 0; ones < 10; ones++)
@@ -16,7 +17,7 @@ for (ones = 0; ones < 10; ones++)
 putchar(tens + '0');
 putchar(ones + '0');
 
- if (tens != 9 || ones != 9)
+if (tens != 9 || ones != 9)
 {
 putchar(',');
 putchar(' ');
