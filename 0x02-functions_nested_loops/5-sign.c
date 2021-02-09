8b6dfe3abfa00862ddc31715a61a85588aaf212c
champer3 <okunolas21@gmail.com>
2021-02-09 09:37:57 +0000

    Functions Nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
-
 /**
- * print_alphabet - Print the alphabet in lowercase.
- *
- * Return: void.
+ * print_sign - Determines if the input
+ * @n: The input number as an integer
+ * Return: 1 is greater than zero. 0 is zero
+ * -1 is less than zero
  */
 
 int print_sign(int n)
