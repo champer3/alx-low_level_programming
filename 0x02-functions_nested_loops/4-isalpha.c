9cf5f22fa6d670b4b6d00d9ba0dbf4590a8b15d4
champer3 <okunolas21@gmail.com>
2021-02-09 09:25:22 +0000

    Functions nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 
 /**
- * print_alphabet - Print the alphabet in lowercase.
- *
- * Return: void.
+ * _isalpha - Print the alphabet in lowercase.
+ * @c: The character in ASCII code
+ * Return: 1 for letters. Retun 0 otherwise
  */
 
 int _isalpha(int c)
 {
-if ((c >= 97 && c <= 122) || (c >= 65 && <= 95))
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 {
 return (1);
 }
