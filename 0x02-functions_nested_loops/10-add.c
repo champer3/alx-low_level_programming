834ca03c58aaf2827d05ad6c5f77c22b59c27e5e
champer3 <okunolas21@gmail.com>
2021-02-09 10:47:14 +0000

    Functions nested loops

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,8 +4,8 @@
  * add - a function that adds two integers
  * add returns the result.
  *
- * a - number one.
- * b - number two.
+ * @a: number one.
+ * @b: number two.
  *
  * Return: Add of number one and number tow
  */
