ddc23aa78db1ae7fb15a5d65ea5a3a3bfa0d2a80
Okunola Stephen <36522298+champer3@users.noreply.github.com>
2021-02-16 07:21:29 +0100

    Create 5-main.c

---
+++ pointer_tests/5-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void modify_my_param(int *m)
+{
+printf("Value of 'm' is: %p\n", m);
+printf("Address of 'm' is: %p\n", &m);
+*m = 402;
+}
+
+int main(void)
+{
+int n;
+int *p;
+
+n = 98;
+p = &n;
+   printf("Value of 'n' before the call: %d\n", n);
+   printf("Address of 'n': %p\n", &n);
+   printf("Value of 'p': %p\n", p);
+   printf("Address of 'p': %p\n", &p);
+   modif_my_param(p);
+   printf("Value of 'n' after the call: %d\n", n);
+   return (0);
+}
