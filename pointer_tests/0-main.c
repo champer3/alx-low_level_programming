1faf445fa06e9fb0080e088ca3418f9bbf173197
Okunola Stephen <36522298+champer3@users.noreply.github.com>
2021-02-16 03:00:51 +0100

    Update 0-main.c

--- pointer_tests/0-main.c
+++ pointer_tests/0-main.c
@@ -2,5 +2,9 @@
 
 int main(void)
 {
-printf("suze of 'char' on my computer: %lu bytes\n", sizeof(char));
+  int n;
+printf("size of 'char' on my computer: %lu bytes\n", sizeof(char));
+printf("size of 'int' on my computer: %lu bytes\n", sizeof(int));
+printf("size of 'float' on my computer: %lu bytes\n", sizeof(float));
+printf("size of variable 'n' on my computer: %lu bytes\n", sizeof(n));
 }
