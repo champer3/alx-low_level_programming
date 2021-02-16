24d98235d93072e37ef8ca40127ac0afeeafb8ab
champer3 <okunolas21@gmail.com>
2021-02-16 02:37:11 +0000

    Pointers

--- pointer_tests/2-main.c
+++ pointer_tests/2-main.c
@@ -4,5 +4,5 @@ int main(void)
 {
   int *p;
 printf("size of the variable '*p' is: %lu\n", sizeof(*p));
-printf("Address of the pointer '*p' is: %p\n", &*p);
+printf("Address of the pointer '*p' is: %p\n", &p);
 }
