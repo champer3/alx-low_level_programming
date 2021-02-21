37dbf85799ff0723d3b755bcf53c694ca9d87020
champer3 <okunolas21@gmail.com>
2021-02-21 03:09:52 +0000

    Practising pointers

--- pointers_trials/2-trial.c
+++ pointers_trials/2-trial.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-#include <string.h>
 
 int _strlen(char *s)
 {
-  printf("%zu\n", strlen(s));
+  int i;
+  for (i = 0, *s != '\0'; i++; s++)
+    {
+      printf("%d\n", i);
+    }return(0);
 }
