5bb5c47778fedfd2f194f215ca3c76a4276b0774
Okunola Stephen <36522298+champer3@users.noreply.github.com>
2021-02-28 12:53:23 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,15 +9,15 @@
   */
 int isnumber(char *s)
 {
-	while (*s)
-	{
-		if (*s < '0' || *s > '9')
-		{
-			return (0);
-		}
-		s++;
-	}
-	return (1);
+while (*s)
+{
+if (*s < '0' || *s > '9')
+{
+return (0);
+}
+s++;
+}
+return (1);
 }
 /**
   * main - adds numbers
@@ -27,24 +27,23 @@ int isnumber(char *s)
   */
 int main(int argc, char *argv[])
 {
-	unsigned int test, count = argc, loop = 1, total = 0;
+unsigned int test, count = argc, loop = 1, total = 0;
 
-	while (loop < count)
-	{
-		test = isnumber(argv[loop]);
-		if (test == 1)
-		{
-			total += atoi(argv[loop]);
-		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
-		loop++;
-	}
-	printf("%d\n", total);
-
-	return (0);
+while (loop < count)
+{
+test = isnumber(argv[loop]);
+if (test == 1)
+{
+total += atoi(argv[loop]);
 }
-© 2021 GitHub, Inc.
+else
+{
+printf("Error\n");
+return (1);
+}
+loop++;
+}
+printf("%d\n", total);
+
+return (0);
+}
