cf4f3ac10cedb4981890f75f785080f9ebfc36b3
Okunola Stephen <36522298+champer3@users.noreply.github.com>
2021-03-08 16:45:52 +0100

    Create 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+  * init_dog - initialize dog struct
+  * @d: struct name
+  * @name: dog name
+  * @age: dog age
+  * @owner: owner name
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
