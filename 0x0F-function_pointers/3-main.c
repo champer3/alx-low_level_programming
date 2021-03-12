7fce47cb980e120885b47add607bd3b1b8a7de01
Okunola Stephen <36522298+champer3@users.noreply.github.com>
2021-03-12 16:51:25 +0100

    Create 3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - entry point of calc program
+  * @argc: number of arguments
+  * @argv: array of arguments
+  * Return: 0 if successful
+  */
+int main(int argc, char *argv[])
+{
+int num1, num2;
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+
+if (get_op_func(argv[2]))
+printf("%d\n", (get_op_func(argv[2])(num1, num2)));
+else
+{
+printf("Error\n");
+exit(99);
+}
+
+return (0);
+}
